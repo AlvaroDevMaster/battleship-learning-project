74a5be786336924c2e6f93b4ccf0383f65f797a2
AlvaroDevMaster <alvaromaquedamf6@gmail.com>
2024-10-18 20:44:40 +0200

    Continuacion de generatePositionsController.php

--- controllers/generatePositionsController.php
+++ controllers/generatePositionsController.php
@@ -2,10 +2,13 @@
 include_once("../includes/vars.php");
 $positions = [];
 foreach($ships as $type => $numberOfShips){
-    array_push($postions, addShip($type, $numberOfShips));
+    for($i = 1; $i <= $numberOfShips; $i++)
+    array_push($postions, addShip($type, $positions));
 }
-function addShip($type, $numberOfShips){
+function addShip($type, $positions){
     $shipLength = 0;
+    $row = 0;
+    $column = 0;
     switch($type){
         case "portaaviones": $shipLength = 5;
         break;
@@ -14,6 +17,22 @@ function addShip($type, $numberOfShips){
         case "submarino": $shipLength = 3;
         break; 
         case "destructor": $shipLength = 2;
+    };
+    //Orientation Logic: 1=>Left,2=>Top,3=>Right,4=>Down 
+    
+    $orientation = rand(0,4);
+    
+    function getRandomRowsAndColumns(){
+        $row = rand(0, 10);
+        $column = rand(0, 10);
+        return [$row, $column];
+    }
+
+    if($orientation === 2 || $orientation === 4){
+
+    }
+    else if($orientation === 1 || $orientation === 3){
+
     }
     return [];
 }
